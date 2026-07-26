ea6304cecbe449f0ec0a4b12ceec778bc8cb20c2
AlyHamdan <alyhamdan20@gmail.com>
2026-07-26 17:52:00 +0300

    first commit :)

--- backend/Servcies/Interfaces/ITokenServices.cs
+++
@@ -1,9 +0,0 @@
-﻿using HotelReservationChatBot.Models.RequestDTO;
-
-namespace HotelReservationChatBot.Servcies.Interfaces
-{
-    public interface ITokenServices
-    {
-        public string GenerateToken(LoginRequestDTO request);
-    }
-}
